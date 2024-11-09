acf9c45a257f209b67ed8c7c582cf77da05e9449
roboty.p.d <freddik1990@gmail.com>
2024-11-09 11:12:30 +0200

    Add new controler

---
+++ Controllers/Hello.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Привет, мир");
+        }
+    }
+}
